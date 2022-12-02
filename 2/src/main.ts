7d962216c0fdc8ea0af4cc7ca7ea2453db16ce24
Darius Elphick <darius.elphick@trademe.co.nz>
2022-12-02 19:58:22 +1300

    Completed part 1

--- 2/src/main.ts
+++ 2/src/main.ts
@@ -1,3 +1,36 @@
-const helloWorld = (): string => ("Hello World!");
+const scoreMap = {
+    'X': {
+        'base': 1,
+        'A': 3,
+        'B': 0,
+        'C': 6
+    },
+    'Y': {
+        'base': 2,
+        'A': 6,
+        'B': 3,
+        'C': 0
+    },
+    'Z': {
+        'base': 3,
+        'A': 0,
+        'B': 6,
+        'C': 3
+    }
+};
 
-export { helloWorld };
+const getRoundResult = (round: Array<string>): number => {
+    const roundScores = scoreMap[round[1]];
+    return roundScores.base + roundScores[round[0]];
+};
+
+const getStrategyScore = (strategy: Array<Array<string>>): number => {
+    return strategy.reduce(
+        (accumilatedScore, currentRound) => {
+            return accumilatedScore + getRoundResult(currentRound);
+        }, 
+        0
+    );
+};
+
+export { getRoundResult, getStrategyScore };
