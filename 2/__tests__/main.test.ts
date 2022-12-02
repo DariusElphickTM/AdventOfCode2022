ca165b7da3aead1673a0d5e58de7f972e7d28a62
Darius Elphick <darius.elphick@trademe.co.nz>
2022-12-02 19:18:37 +1300

    Written function to convert puzzle input to array

--- 2/__tests__/main.test.ts
+++ 2/__tests__/main.test.ts
@@ -1,7 +1,24 @@
-import { helloWorld } from '../src/main.js';
+//import { getStrategyScore } from '../src/main.js';
 
-describe('Hello world', () => {
-  it('should return Hello World', () => {
-    expect(helloWorld()).toBe("Hello World!");
+const convertPuzzleInputFromStringToArray = (puzzleInput: string): Array<Array<string>> => (puzzleInput.split('\n').map((currentRound) => currentRound.trim().split(' ')));
+
+describe('puzzleInputHelper', () => {
+  it('should convert puzzle input from a string to an array', () => {
+    const testInput = `A Y
+    B X
+    C Z`;
+    const expectedOutput = [
+      ['A', 'Y'],
+      ['B', 'X'],
+      ['C', 'Z']
+    ];
+
+    expect(convertPuzzleInputFromStringToArray(testInput)).toEqual(expectedOutput);
   });
+});
+
+describe('getStrategyScore', () => {
+  /*it('should return return the correct score for the test payload', () => {
+    expect(getStrategyScore(testStrategy)).toBe("Hello World!");
+  });*/
 });
