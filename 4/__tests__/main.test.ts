4ce011f3abafe9d78353648db66ffbcde30ac036
Darius Elphick <darius.elphick@trademe.co.nz>
2022-12-04 18:30:29 +1300

    Created helper to process puzzle input

--- 4/__tests__/main.test.ts
+++ 4/__tests__/main.test.ts
@@ -1,7 +1,57 @@
-import { helloWorld } from '../src/main.js';
+//import { helloWorld } from '../src/main.js';
 
-describe('Hello world', () => {
-  it('should return Hello World', () => {
-    expect(helloWorld()).toBe("Hello World!");
+const testInput = `2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8`;
+
+//const puzzleInput = ``;
+
+const convertTestInputToArrayOfElfPairs = (input: string): Array<Array<Array<number>>> => {
+  return input.split('\n').map(
+    (elfPair) => (
+      elfPair.split(',').map(
+        (elfSection) => (
+          elfSection.split('-').map(
+            (digitAsString) => (+digitAsString)
+          )
+        )
+      )
+    )
+  );
+};
+
+describe('convertTestInputTo...', () => {
+  it('should convert the test input into an array of elf pairs', () => {
+    const expectedOutput = [
+      [
+        [2, 4],
+        [6, 8]
+      ],
+      [
+        [2, 3],
+        [4, 5]
+      ],
+      [
+        [5, 7],
+        [7, 9]
+      ],
+      [
+        [2, 8],
+        [3, 7]
+      ],
+      [
+        [6, 6],
+        [4, 6]
+      ],
+      [
+        [2, 6],
+        [4, 8]
+      ]
+    ];
+
+    expect(convertTestInputToArrayOfElfPairs(testInput)).toEqual(expectedOutput);
   });
 });
