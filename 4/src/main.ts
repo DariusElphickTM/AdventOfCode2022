5930eab877f79e491f1205d7d58a1410faef4ec5
Darius Elphick <darius.elphick@trademe.co.nz>
2022-12-04 18:49:59 +1300

    Completed step 1

--- 4/src/main.ts
+++ 4/src/main.ts
@@ -1,3 +1,23 @@
-const helloWorld = (): string => ("Hello World!");
+const isCrossOver = (elfPair: Array<Array<number>>): boolean => {
+    const firstSection = elfPair[0];
+    const secondSection = elfPair[1];
+    return (
+        (
+            (firstSection[0] >= secondSection[0]) && 
+            (firstSection[1] <= secondSection[1])
+        ) ||
+        (
+            (secondSection[0] >= firstSection[0]) && 
+            (secondSection[1] <= firstSection[1])
+        )
+    );
+}
 
-export { helloWorld };
+const getCrossOverRangeCount = (elfPairs: Array<Array<Array<number>>>): number => {
+    return elfPairs.reduce((crossOverCount, currentElfPair) => {
+        currentElfPair;
+        return isCrossOver(currentElfPair) ? crossOverCount + 1 : crossOverCount;
+    }, 0);
+};
+
+export { getCrossOverRangeCount, isCrossOver };
