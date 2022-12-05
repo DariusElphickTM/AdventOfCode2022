00e5ddc95b5477916bb62f069f12acc0b6ccea51
Darius Elphick <darius.elphick@trademe.co.nz>
2022-12-05 21:18:14 +1300

    Added a function to extract the moves from the input

--- 5/__tests__/main.test.ts
+++ 5/__tests__/main.test.ts
@@ -1,7 +1,37 @@
-import { helloWorld } from '../src/main.js';
+//import { helloWorld } from '../src/main.js';
 
-describe('Hello world', () => {
-  it('should return Hello World', () => {
-    expect(helloWorld()).toBe("Hello World!");
+const testInput = `    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2`;
+
+//const puzzleInput = ``;
+
+const getMovesFromInput = (input: string): Array<Array<number>> => (
+  [...input.matchAll(new RegExp(/move \d from \d to \d/, 'g'))].map(
+    (currentMove) => (
+      [...currentMove[0].matchAll(new RegExp(/\d/, 'g'))].map(
+        (currentDigit) => (
+          +currentDigit[0]
+        )
+      )
+    )
+  )
+);
+
+describe('getMovesFromInput', () => {
+  it('should return a set of moves as a data structure', () => {
+    const expectedOutput = [
+      [1,2,1],
+      [3,1,3],
+      [2,2,1],
+      [1,1,2]
+    ];
+    expect(getMovesFromInput(testInput)).toEqual(expectedOutput);
   });
 });
