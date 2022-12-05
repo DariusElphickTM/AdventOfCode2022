35fbf04dbfebd56d897f18e96503543be037ad43
Darius Elphick <darius.elphick@trademe.co.nz>
2022-12-06 08:33:01 +1300

    Written an (admitedly horrible) function to parse the stacks

--- 5/__tests__/main.test.ts
+++ 5/__tests__/main.test.ts
@@ -12,6 +12,25 @@ move 1 from 1 to 2`;
 
 //const puzzleInput = ``;
 
+const getStacksFromInput = (input: string): Array<Array<string>> => {
+  const inputArray = input.substring(0, input.indexOf('1') - 2).split('\n').reverse();
+  const outputArray = [...inputArray[0].matchAll(new RegExp(/\[[A-Z]\]/, 'g'))].map(
+    (currentStack) => ([currentStack[0]])
+  );
+
+  for(let i = 1; i < inputArray.length; i++) {
+    const currentStackLevel = inputArray[i];
+    for(let j = 0; j * 4 < currentStackLevel.length; j += 1) {
+      const stringPosition = j * 4;
+      const isThereABlockHere = currentStackLevel.substring(stringPosition, stringPosition + 3).match(new RegExp(/\[[A-Z]\]/));
+      if(isThereABlockHere !== null) {
+        outputArray[j].push(isThereABlockHere[0]);
+      }
+    }
+  }
+  return outputArray;
+};
+
 const getMovesFromInput = (input: string): Array<Array<number>> => (
   [...input.matchAll(new RegExp(/move \d from \d to \d/, 'g'))].map(
     (currentMove) => (
@@ -23,6 +42,17 @@ const getMovesFromInput = (input: string): Array<Array<number>> => (
     )
   )
 );
+
+describe('getStacksFromInput', () => {
+  it('should return a set of stacks ad a data structure', () => {
+    const expectedOutput = [
+      ['[Z]', '[N]'],
+      ['[M]', '[C]', '[D]'],
+      ['[P]']
+    ];
+    expect(getStacksFromInput(testInput)).toEqual(expectedOutput);
+  });
+});
 
 describe('getMovesFromInput', () => {
   it('should return a set of moves as a data structure', () => {
