ca93d354875ceffbbbe5e03afce659b61bd739c4
Darius Elphick <darius.elphick@trademe.co.nz>
2022-12-06 20:44:04 +1300

    Completed step 1

--- 6/src/main.ts
+++ 6/src/main.ts
@@ -1,3 +1,18 @@
-const helloWorld = (): string => ("Hello World!");
+const detectStartOfPacket = (buffer: string): number | null => {
+    const bufferDetector = [];
+    for(let i = 0; i < buffer.length; i++) {
+        const currentLetter = buffer[i];
+        if(bufferDetector.includes((currentLetter))) {
+            bufferDetector.splice(0, bufferDetector.indexOf(currentLetter) + 1);
+            bufferDetector.push(currentLetter);
+        } else {
+            bufferDetector.push(currentLetter);
+            if(bufferDetector.length == 4) {
+                return i + 1;
+            }
+        }
+    }
+    return 0;
+};
 
-export { helloWorld };
+export { detectStartOfPacket };
